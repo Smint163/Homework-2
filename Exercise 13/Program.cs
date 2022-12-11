b27171647d3982a75968f5775edb6e8730a407d1
Smint <smint-russia@mail.ru>
2022-12-11 22:05:25 +0300

    Exercise 13

---
+++ Exercise 13/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Введите любое число");
+string num = Console.ReadLine();
+int number = Convert.ToInt32(num);
+
+
+if (number > 99)
+{
+    string str = number.ToString();
+    Console.WriteLine($"{str[2]}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
