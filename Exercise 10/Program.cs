92bd39ebc04be753d47bfd390983361296641621
Smint <smint-russia@mail.ru>
2022-12-11 21:44:49 +0300

    Exercise 10

---
+++ Exercise 10/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число");
+string num= Console.ReadLine();
+int number = Convert.ToInt32(num);
+
+{
+    int result = ((number - (((number / 100) * 100) + (number % 10))) / 10);
+    Console.WriteLine($"{result}");
+}
