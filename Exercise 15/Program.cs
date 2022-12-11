d264f660930fa35aab2e90e438814667dc094481
Smint <smint-russia@mail.ru>
2022-12-11 23:30:53 +0300

    For check

---
+++ Exercise 15/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите номер дня недели");
+string num = Console.ReadLine();
+int number = Convert.ToInt32(num);
+
+if ((number >= 1) && (number <= 5))
+{
+     Console.WriteLine("нет");
+}
+
+else if ((number == 6) || (number == 7))
+{
+    Console.WriteLine("да");
+}
+
+else
+{
+    Console.WriteLine("Такого дня недели нет");
+}
